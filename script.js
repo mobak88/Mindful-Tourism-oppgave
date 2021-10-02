18a7a1cf352963da9ffb3138e919663a517305cd
mobak88 <mostafa0401@ymail.com>
2021-10-02 15:49:21 +0200

    Final adjustments

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const toggleButton = document.getElementsByClassName(
   "header-menu__hamburger"
 )[0];
 const navbarLinks = document.getElementsByClassName("mobile-menu")[0];
+const form = document.getElementsByClassName("contact-form");
 
 toggleButton.addEventListener("click", (e) => {
-  e.preventDefault(e);
+  e.preventDefault();
   navbarLinks.classList.toggle("mobile-menu__active");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
