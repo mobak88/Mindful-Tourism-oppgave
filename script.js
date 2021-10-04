aefb19de552ec13cfa79e74d3059e78579c82244
mobak88 <mostafa0401@ymail.com>
2021-10-04 10:48:54 +0200

    Fix js error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleButton = document.getElementsByClassName(
   "header-menu__hamburger"
 )[0];
 const navbarLinks = document.getElementsByClassName("mobile-menu")[0];
-const form = document.getElementsByClassName("contact-form");
+const form = document.getElementById("form");
 
 toggleButton.addEventListener("click", (e) => {
   e.preventDefault();
